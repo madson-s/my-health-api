5147c6a4d7a6da6c02c8950a3257a8b5a0c3935e
Madson Silva <madsonoliveirasilva@gmail.com>
2023-04-24 10:29:03 -0300

    refactor: add passwoord hashing on user creation

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -5,11 +5,15 @@ import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { SignupUserDto } from './dtos/signup-user.dto';
 import { GetUserDto } from './dtos/get-user.dto';
 import { excludingFieldsHelper } from 'src/helpers/excluding-fields-helper';
+import { HashService } from 'src/services/hash.service';
 
 @Controller('user')
 @ApiTags('user')
 export class UsersController {
-  constructor(private readonly userService: UsersService) {}
+  constructor(
+    private readonly hashService: HashService,
+    private readonly userService: UsersService,
+  ) {}
 
   @Post('')
   @ApiResponse({ status: 200, type: GetUserDto })
@@ -17,10 +21,19 @@ export class UsersController {
     @Body()
     userData: SignupUserDto,
   ): Promise<Omit<UserModel, 'password'>> {
-    const user = await this.userService.createUser(userData);
+    const { password } = userData;
+
+    const hashedPassword = await this.hashService.hash(password);
+
+    const user = await this.userService.createUser({
+      ...userData,
+      password: hashedPassword,
+    });
+
     const userWithoutPassword = excludingFieldsHelper.exclude(user, [
       'password',
     ]);
+
     return userWithoutPassword;
   }
 }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { PrismaService } from 'src/database/prisma.service';
+import { HashService } from 'src/services/hash.service';
 
 @Module({
   controllers: [UsersController],
-  providers: [PrismaService, UsersService],
+  providers: [HashService, PrismaService, UsersService],
   imports: [],
 })
 export class UsersModule {}
