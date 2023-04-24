4450d4eb955817435201752b699142d7e2c549c3
Madson Silva <madsonoliveirasilva@gmail.com>
2023-04-24 12:00:55 -0300

    feat: add jwt token on user login response

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,18 +1,19 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { User as UserModel } from '@prisma/client';
-import { AuthService } from './auth.service';
+import { Body, Controller, Post, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { excludingFieldsHelper } from 'src/helpers/excluding-fields-helper';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { GetUserDto } from 'src/users/dtos/get-user.dto';
 import { LoginUserDto } from './dtos/login-user.dto';
 import { HashService } from 'src/services/hash.service';
+import { UsersService } from 'src/users/users.service';
 
 @Controller('auth')
 @ApiTags('auth')
 export class AuthController {
   constructor(
+    private readonly usersService: UsersService,
     private readonly hashService: HashService,
-    private readonly authService: AuthService,
+    private readonly jwtService: JwtService,
   ) {}
 
   @Post('authenticate')
@@ -22,24 +23,28 @@ export class AuthController {
     userData: LoginUserDto,
   ): Promise<any> {
     const { email, password } = userData;
-    const user = await this.authService.login({
+    const user = await this.usersService.getUser({
       where: {
         email,
       },
     });
 
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+
     const authorized = await this.hashService.compare(password, user.password);
 
-    if (!authorized)
-      return {
-        statusCode: 401,
-        message: 'Usuário ou senha invalido!',
-        error: 'Unauthorized',
-      };
+    if (!authorized) {
+      throw new UnauthorizedException();
+    }
 
     const userWithoutPassword = excludingFieldsHelper.exclude(user, [
       'password',
     ]);
-    return userWithoutPassword;
+
+    const access_token = await this.jwtService.signAsync({ sub: user.id });
+
+    return { ...userWithoutPassword, access_token };
   }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
 import { PrismaService } from 'src/database/prisma.service';
 import { HashService } from 'src/services/hash.service';
+import { JwtModule } from '@nestjs/jwt';
+import { UsersModule } from 'src/users/users.module';
+import { UsersService } from 'src/users/users.service';
+import { jwtConstants } from './constants';
 
 @Module({
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      global: true,
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [PrismaService, HashService, AuthService],
+  providers: [UsersService, PrismaService, HashService],
 })
 export class AuthModule {}
