312b670dcdc954c5e87ab5f75481d7f1c4aaedb6
Madson Silva <madsonoliveirasilva@gmail.com>
2023-04-29 19:11:14 -0300

    feat: add reset module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ArticlesModule } from './articles/articles.module';
+import { ResetModule } from './reset/reset.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, ArticlesModule],
+  imports: [AuthModule, ResetModule, UsersModule, ArticlesModule],
   controllers: [],
   providers: [],
 })
